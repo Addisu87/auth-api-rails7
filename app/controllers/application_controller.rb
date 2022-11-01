3bec944ca70db974136a4b01ee4a7a78eb83a79f
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-11-01 09:33:03 +0300

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
+  end
 end
